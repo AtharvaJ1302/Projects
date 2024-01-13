079f4144867e3774c2d49d09f0b43c15263896ea
AtharvaJoshi1302 <110755376+AtharvaJ1302@users.noreply.github.com>
2024-01-13 10:16:47 +0530

    Initial commit

---
+++ Instagram bot/instaBot.py
@@ -0,0 +1,5 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username = "bua_pvt",password = "Riojos1508@$")
+# bot.follow("wscubetechindia")  ==> follow an account with given username
+bot.upload_photo()
